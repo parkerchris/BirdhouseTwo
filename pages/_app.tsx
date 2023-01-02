70c9743ecd3f15c02e312c4ed0fcd5ddfd38620c
Chris Parker <cwbparker@gmail.com>
2023-01-02 10:30:00 -0800

    Apollo client setup.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,16 @@
+import { ApolloProvider } from "@apollo/client"
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { SessionProvider } from 'next-auth/react'
+import { client } from "../graphql/apollo-client"
+
 
 export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
+    <ApolloProvider client={client}>
+      <SessionProvider session={session}>
+        <Component {...pageProps} />
+      </SessionProvider>
+    </ApolloProvider>
   )
 }
